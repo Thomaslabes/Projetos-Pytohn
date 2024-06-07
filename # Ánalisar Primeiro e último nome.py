3c2f8bf0f8b6ecf15d1a63bd500d11edd712f1fa
Thomas de Souza Krieck Labes <thomasdesouza30@gmail.com>
2024-06-07 14:14:01 -0300

    Delete # Ánalisar Primeiro e último nome.py

--- # Ánalisar Primeiro e último nome.py
+++
@@ -1,5 +0,0 @@
-# Ánalisar Primeiro e último nome 
-nome = str(input('Digite seu nome completo:')).strip()
-nome = nome.split()
-print('Seu primerio nome é {}.' .format(nome[0]))
-print('Seu último nome é {}.' .format(nome [len (nome) -1]))
