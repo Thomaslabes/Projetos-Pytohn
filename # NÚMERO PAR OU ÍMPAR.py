523f7c8f894a229e43d976513181272517356753
Thomas de Souza Krieck Labes <thomasdesouza30@gmail.com>
2024-06-07 14:12:20 -0300

    Delete # NÚMERO PAR OU ÍMPAR.py

--- # NÚMERO PAR OU ÍMPAR.py
+++
@@ -1,9 +0,0 @@
-# NÚMERO PAR OU ÍMPAR
-
-numero = int(input('Digite um número: '))
-resultado = numero % 2
-
-if resultado == 0:
-    print(' O número {} é Par' .format(numero))
-else:
-    print('O número {} é Ímpar' .format(numero))
